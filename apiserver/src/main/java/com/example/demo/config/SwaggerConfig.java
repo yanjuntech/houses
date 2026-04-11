0b2207213c15d8e4b4ccfa56072840d9fdcdfabf
microero <microero@users.noreply.github.com>
2026-04-11 04:25:31 +0000

    feat: 集成Knife4j增强API文档功能

---
+++ apiserver/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("房屋租赁系统API")
+                .description("房屋租赁系统的RESTful API文档")
+                .contact(new Contact("开发者", "https://github.com", "developer@example.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
